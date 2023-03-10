e660f5c91ab36631570ffa647ba6f300f8a200b7
Alexbarile <alexbarile93fg@yahoo.it>
2023-03-10 13:22:24 +0100

    aggiunta funzione SHOW di PostController Api e aggiunta relativa Route

--- app/Http/Controllers/Api/PostController.php
+++ app/Http/Controllers/Api/PostController.php
@@ -18,4 +18,21 @@ class PostController extends Controller
             'posts' => $posts,
         ]);
     }
+
+    public function show($slug){
+        $post = Post::with('type', 'technologies')->where('slug', $slug)->first();
+
+        if($post){
+            return response()->json([
+                'success' => true,
+                'post' => $post,
+            ]);
+        }
+        else{
+            return response()->json([
+                'success' => false,
+                'error' => 'Nessun post trovato',
+            ]);
+        }
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -17,6 +17,7 @@ use App\Http\Controllers\Api\PostController as PostController;
 */
 
 Route::get('/posts', [PostController::class, 'index']);
+Route::get('/posts/{slug}', [PostController::class, 'show']);
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
