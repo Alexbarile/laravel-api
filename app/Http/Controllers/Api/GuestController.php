f75ee13df5658c840a35d907e2e612adff9a9b11
Alexbarile <alexbarile93fg@yahoo.it>
2023-03-13 17:48:26 +0100

    creati GuestController, Model GuestLead, GuestContact per email e guest_email.blade.php

--- app/Http/Controllers/Api/GuestController.php
+++ app/Http/Controllers/Api/GuestController.php
@@ -7,10 +7,48 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Mail;
 use Illuminate\Support\Facades\Validator;
 
+// MODEL
+use App\Models\GuestLead;
+
+use App\Mail\GuestContact;
 
 class GuestController extends Controller
 {
     public function store(Request $request){
-        
+        $data = $request->all();
+
+        // VALIDIAMO 
+        $validator = Validator::make($data,
+        [
+            'name' => 'required',
+            'surname' => 'required',
+            'phone' => 'required',
+            'email' => 'required',
+            'message' => 'required'
+
+        ]);
+
+        // SE FALLISCE LA VALIDAZIONE
+
+        if($validator->fails()){
+            return response()->json([
+                'success' => false,
+                'errors' => $validator->errors()
+            ]);
+        }
+
+        // ALTRIMENTI PROCEDE AVANTI E SALVA NEL DB LE INFORMAZIONI INSERITE NELLA EMAIL
+
+        $newContact = new GuestLead();
+        $newContact->fill($data);
+
+        $newContact->save();
+        Mail::to('hello@example.com')->send(new GuestContact($newContact));
+
+        return response()->json([
+            'success' => true
+        ]);
     }
+
+    
 }
---
+++ resources/views/emails/guest_email.blade.php
@@ -0,0 +1,7 @@
+<h1>Nuovo Contatto da BoolPress</h1>
+
+<p>Nome: {{$lead->name}}</p>
+<p>Cognome: {{$lead->surname}}</p>
+<p>Telefono: {{$lead->phone}}</p>
+<p>Email: {{$lead->email}}</p>
+<p>Contenuto: {{$lead->message}}</p>
