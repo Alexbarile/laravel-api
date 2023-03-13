bdfa4dabfb37c2c2a8dbee3fe0e4983aed24746f
Alexbarile <alexbarile93fg@yahoo.it>
2023-03-13 17:22:04 +0100

    new GuestController in Api

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\PostController as PostController;
+use App\Http\Controllers\Api\GuestController as GuestController;
 
 
 /*
@@ -18,6 +19,7 @@ use App\Http\Controllers\Api\PostController as PostController;
 
 Route::get('/posts', [PostController::class, 'index']);
 Route::get('/posts/{slug}', [PostController::class, 'show']);
+Route::get('/contacts', [GuestController::class, 'store']);
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
